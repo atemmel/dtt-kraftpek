431d6b02a7c0d19528432393f1a71508ae4298ba
Fredrik Kåhre <fredrik.kahre@bolagsverket.se>
2023-11-07 16:08:42 +0100

    börjar parsing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/atemmel/dtt-kraftpek
 
 go 1.21.0
+
+require github.com/yuin/goldmark v1.6.0 // indirect
--- pkg/md/md.go
+++ pkg/md/md.go
@@ -1,5 +1,15 @@
 package md
 
+import (
+	"github.com/yuin/goldmark"
+)
+
 func MyFunc() bool {
 	return true
+}
+
+func Parse_MD() {
+
+	md := goldmark.DefaultParser()
+	_ = md
 }
